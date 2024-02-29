028f5c7e93e4e6b0dc6c5f9948c9ed12ecd308ce
unknown <TanzidAhsan>
2024-02-29 10:31:33 +0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Tanzid</Text>
+      <Text>Habib</Text>
+      <Text>Tanzid</Text>
+      <Text>Tanzid</Text>
+      <Text>Tanzid</Text>
+      
+
+      
+    </View>
+  )
+}
+
+export default App
